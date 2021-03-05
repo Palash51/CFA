d990f35bede8bf49d93e5819d5497beea4e38dce
Palash51 <palashpatidar51@gmail.com>
2021-03-06 00:07:29 +0530

    add react hot loader

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import React from 'react';
 import './index.css';
 import { render } from 'react-dom';
 import App from './app';
+import { AppContainer } from 'react-hot-loader';
+
 
 render(
-<>
+<AppContainer>
         <App />
-</>
+</AppContainer>
 , document.getElementById('root'));
