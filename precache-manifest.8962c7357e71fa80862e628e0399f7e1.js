bca8d651d98ef0569a5242cfb57f6d2485ccbd54
Palash Patidar <palashpatidar51@gmail.com>
2021-11-05 14:52:55 +0530

    Updates

---
+++ precache-manifest.8962c7357e71fa80862e628e0399f7e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472d44f996fd668d2473edf9360db754",
+    "url": "/CFA/index.html"
+  },
+  {
+    "revision": "bf172e68c338580f6433",
+    "url": "/CFA/static/css/2.0e8450bb.chunk.css"
+  },
+  {
+    "revision": "d011036b82ffe2b90161",
+    "url": "/CFA/static/css/main.2051c70e.chunk.css"
+  },
+  {
+    "revision": "bf172e68c338580f6433",
+    "url": "/CFA/static/js/2.2ef11421.chunk.js"
+  },
+  {
+    "revision": "4fa5769a01255b9104f0a0e388fc4f54",
+    "url": "/CFA/static/js/2.2ef11421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d011036b82ffe2b90161",
+    "url": "/CFA/static/js/main.e9267ebd.chunk.js"
+  },
+  {
+    "revision": "1d2049597d670f8d91b4",
+    "url": "/CFA/static/js/runtime-main.a97ef9cf.js"
+  },
+  {
+    "revision": "548cdd0e4a7d21c2a9c3bc8580254239",
+    "url": "/CFA/static/media/MaterialIcons-Regular.548cdd0e.ttf"
+  },
+  {
+    "revision": "4b937d057e6da6a4fac02b42784fdb6a",
+    "url": "/CFA/static/media/MaterialIconsOutlined-Regular.4b937d05.otf"
+  },
+  {
+    "revision": "f8d5e69de9cdc901dab69da2d6463217",
+    "url": "/CFA/static/media/MaterialIconsRound-Regular.f8d5e69d.otf"
+  },
+  {
+    "revision": "5257f80b95fdba0a233f61fef096b3f2",
+    "url": "/CFA/static/media/MaterialIconsSharp-Regular.5257f80b.otf"
+  },
+  {
+    "revision": "f2472d5074320cb86f3db2bbf403939d",
+    "url": "/CFA/static/media/MaterialIconsTwoTone-Regular.f2472d50.otf"
+  }
+]);
